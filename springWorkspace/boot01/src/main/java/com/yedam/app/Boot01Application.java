0dae750c690c6258105bdb6068c9d20b24738ffd
daeun <ekdms7059@naver.com>
2024-07-09 17:11:57 +0900

    dddd

--- springWorkspace/boot01/src/main/java/com/yedam/app/Boot01Application.java
+++ springWorkspace/boot01/src/main/java/com/yedam/app/Boot01Application.java
@@ -1,9 +1,11 @@
 package com.yedam.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.yedam.app.**.mapper")
 public class Boot01Application {
 
 	public static void main(String[] args) {
